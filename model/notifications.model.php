2395a3eba4c8b8d24e81aa67192a6665cbfb37a2
karlosdraco <karlosdraco27@yahoo.com>
2019-06-17 23:03:32 +0800

    Fixed RIGHT JOIN in notifications query

--- model/notifications.model.php
+++ model/notifications.model.php
@@ -26,22 +26,29 @@ require_once './config/DB.php';
 
         public function fetchNotification($loggedUserId){
             
-            $statement = $this->conn->query("SELECT * FROM notifications WHERE agent_id=:aid");
-            $statement->bindParam(':aid', $loggedUserId);
+            $statement = $this->conn->query("SELECT * FROM notifications WHERE user_id=:uid");
+            $statement->bindParam(':uid', $loggedUserId);
             
             if($statement->execute()){
                 if($statement->rowCount() > 0){
                    $data = $statement->fetch();
-                   $uid = $data['agent_id'];
+                   $uid = $data['user_id'];
 
-                   $statement = $this->conn->query("SELECT notifications.*,users.firstname, users.lastname, users.profile_image
-                    FROM notifications RIGHT JOIN users ON notifications.agent_id=users.id
-                    RIGHT JOIN food_post ON notifications.food_id=food_post.id WHERE notifications.user_id=:uid ORDER BY notifications.notif_date DESC");
+                   $statement = $this->conn->query("SELECT notifications.*,food_post.food_name,users.firstname,
+                   users.lastname,users.profile_image
+                    FROM notifications RIGHT JOIN food_post ON notifications.food_id=food_post.id
+                    RIGHT JOIN users ON notifications.agent_id=users.id
+                    WHERE notifications.user_id=:uid ORDER BY notifications.notif_date DESC");
                    $statement->bindParam(':uid', $uid);
                    $statement->execute();
                    return $statement->fetchAll(PDO::FETCH_ASSOC);
                 }
             }
+        }
+
+        public function notificationCount($loggedUserId){
+            $statement = $this->conn->query("SELECT * FROM notifications WHERE agent_id=:aid");
+            $statement->bindParam(':aid', $loggedUserId);
         }
 
 
